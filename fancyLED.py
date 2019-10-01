a815f4bc123707553aed73b2dd3b1730c15b43ec
Abigail B Paquette <apaquet37@ccs.local>
2019-10-01 10:05:21 -0400

    Update with pins defined

--- fancyLED.py
+++ fancyLED.py
@@ -1,6 +1,13 @@
-class fancyLED:
-    def __init__(self,)
-    def alternate(self)
+class FancyLED:
+    def __init__(self,pin1,pin2,pin3):
+        self.pin1 = digitalio.DigitalInOut(pin1)
+        self.pin1.direction = digitalio.Direction.OUTPUT
+        self.pin2 = digitalio.DigitalInOut(pin2)
+        self.pin2.direction = digitalio.Direction.OUTPUT
+        self.pin3 = digitalio.DigitalInOut(pin3)
+        self.pin3.direction = digitalio.Direction.OUTPUT
+    def alternate(self):
+
     def blink(self)
     def chase(self)
     def sparkle(self)
