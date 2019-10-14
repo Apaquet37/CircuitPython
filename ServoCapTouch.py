0c2f31a13b93d1f00c1e025b60215cbdf6467589
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2019-10-14 09:56:11 -0400

    Update ServoCapTouch.py

--- ServoCapTouch.py
+++ ServoCapTouch.py
@@ -1,16 +1,16 @@
 import time
 import board
 import pulseio
-import touchio
+import touchio # This is the module necessary to make capacitive touch work
 from adafruit_motor import servo
 
-touch_A1 = touchio.TouchIn(board.A1)
-touch_A2 = touchio.TouchIn(board.A2)
+touch_A1 = touchio.TouchIn(board.A1) #These are the two pins that I just have wires sticking out of
+touch_A2 = touchio.TouchIn(board.A2) #When you touch either of the two wires, it grounds and you can make something happen
 
 # create a PWMOut object on Pin A2.
-pwm = pulseio.PWMOut(board.D9, duty_cycle=2 ** 15, frequency=50)
+pwm = pulseio.PWMOut(board.D9, duty_cycle=2 ** 15, frequency=50) #This is the setup for my servo, it's similar to that of an LED
 
-angle = 90
+angle = 90 # Starting the servo in the middle
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
 
