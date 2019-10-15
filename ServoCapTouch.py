99aba685ca499bea59b17267c25e2edf2be79738
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2019-10-14 21:23:38 -0400

    Add comments to ServoCapTouch.py

--- ServoCapTouch.py
+++ ServoCapTouch.py
@@ -1,8 +1,8 @@
 import time
 import board
-import pulseio
+import pulseio # This module makes pwm objects work
 import touchio # This is the module necessary to make capacitive touch work
-from adafruit_motor import servo
+from adafruit_motor import servo # Getting the servo library so its commands will work
 
 touch_A1 = touchio.TouchIn(board.A1) #These are the two pins that I just have wires sticking out of
 touch_A2 = touchio.TouchIn(board.A2) #When you touch either of the two wires, it grounds and you can make something happen
@@ -10,19 +10,19 @@ touch_A2 = touchio.TouchIn(board.A2) #When you touch either of the two wires, it
 # create a PWMOut object on Pin A2.
 pwm = pulseio.PWMOut(board.D9, duty_cycle=2 ** 15, frequency=50) #This is the setup for my servo, it's similar to that of an LED
 
-angle = 90 # Starting the servo in the middle
+angle = 90 # Starting the servo in the middle, not moving
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
 
 while True:
-    if touch_A1.value:
-        if angle <= 179:
+    if touch_A1.value: # If the first wire is touched
+        if angle <= 179: # and the angle isn't too big
+            my_servo.angle = angle 
+            time.sleep(.03)
+            angle = angle+1 # Move the servo one direction
+    if touch_A2.value: # If the other wire is touched
+        if angle >= 1: # and the angle isn't too small
             my_servo.angle = angle
             time.sleep(.03)
-            angle = angle+1
-    if touch_A2.value:
-        if angle >= 1:
-            my_servo.angle = angle
-            time.sleep(.03)
-            angle = angle-1
+            angle = angle-1 # Move the servo the other direction
 
