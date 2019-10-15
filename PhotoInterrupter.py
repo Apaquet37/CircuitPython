151a295f28ac9544659ecd8db16287dd77be6452
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2019-10-15 10:22:10 -0400

    Add comments to PhotoInterrupter.py

--- PhotoInterrupter.py
+++ PhotoInterrupter.py
@@ -6,17 +6,19 @@ photo = DigitalInOut(board.D13)
 photo.direction = Direction.INPUT
 photo.pull = Pull.UP
 counter = 0
-initial = time.monotonic()
+initial = time.monotonic() #Time monotonic allows you to count how much time has passed since the sketch ran, initial is a variable I'm
+                           #making to use later on
 
 while True:
-    now = time.monotonic()
-    if now - initial > 4:
-        print("The number of interrupts is ", counter)
-        initial = now
+    now = time.monotonic() #This variable updates everytime the while true loop runs, making it the current time
+    if now - initial > 4: #If the current time minus the time this loop last ran is more than 4, run again
+                          #Basically just making the loop run every 4 seconds
+        print("The number of interrupts is ", counter) #Printing the number of interuppts to the serial monitor every four seconds
+        initial = now #Initial gets set to the current time at the end of very loop where you print
 
-    if not photo.value:
+    if not photo.value: #This loop goes when the photo interrupter hasn't been interrupted
         time.sleep(.25)
 
-    else:
-        counter = counter + 1
+    else: #This is what happens when the photo interrupter is triggered
+        counter = counter + 1 #Adding one to the counter so that an accurate number of interrupts can be printed
         time.sleep(.25)
