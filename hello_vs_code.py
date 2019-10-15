784145b459203859e4b1ea7451358ee6b0132913
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2019-10-15 10:34:26 -0400

    Add comments to hello_vs_code.py

--- hello_vs_code.py
+++ hello_vs_code.py
@@ -2,5 +2,6 @@ import board
 import time
 
 while True:
-    print("Hello world!")
+    print("Hello world!") #Printing Hello World to the serial monitor every second
     time.sleep(1)
+#This was just an assignment to get used to a new IDE, VS Code
